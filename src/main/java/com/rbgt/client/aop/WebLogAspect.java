20b986e7856f80b347df5b2ec56ce8b206c030dc
俞春旺 <1228368500@qq.com>
2020-03-25 21:17:49 +0800

    新增枚举使用

--- src/main/java/com/rbgt/client/aop/WebLogAspect.java
+++ src/main/java/com/rbgt/client/aop/WebLogAspect.java
@@ -14,14 +14,8 @@ import java.util.Arrays;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.rbgt.client.config.JwtUserDetailsService;
-import com.rbgt.client.data.ResponseCode;
-import com.rbgt.client.exception.BaseException;
-import com.rbgt.client.util.JwtTokenUtil;
-import io.jsonwebtoken.ExpiredJwtException;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -31,33 +25,10 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
-
-import javax.servlet.http.HttpServletRequest;
 
 
 @Aspect
@@ -66,11 +37,6 @@ public class WebLogAspect {
 
     private final Logger logger = LoggerFactory.getLogger(WebLogAspect.class);
 
-    @Autowired
-    private JwtUserDetailsService jwtUserDetailsService;
-
-    @Autowired
-    private JwtTokenUtil jwtTokenUtil;
     /**
      * 切入点描述 这个是controller包的切入点
      */
